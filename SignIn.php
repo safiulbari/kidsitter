adf9e2e9c0fc0fc27986c2146c81bb3c88833ba9
Shahzadi Khadija Khan Himu <100446779+shahzadi112@users.noreply.github.com>
2022-04-16 12:30:07 +0600

    SignIn_Users
    SignIn for clients

---
+++ SignIn.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>SignIn</title>
+    </head>
+    
+    <body>
+        <h4>SignIn</h4>
+        <form action="SignInprocess.php" method="POST">
+            <label for="myemail">Email</label>:
+            <input type="email" id="myemail" name="myemail" placeholder="x@gmail.com">
+            <br>
+            <label for="mypass">Password</label>:
+            <input type="password" id="mypass" name="mypass">
+            <br>
+            <input type="submit" value="Click to SignIn">
+             <style>
+                  body{
+                         
+                          background-color : slategray;
+                          background-attachment : fixed;
+                          background-repeat : no-repeat;
+                          background-size : cover;
+}
+</style>
+        </form>
+    </body>
+</html>
---
+++ SignInprocess.php
@@ -0,0 +1,71 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD']=="POST"){
+    //we will process here
+    
+    if(   isset($_POST['myemail']) 
+       && isset($_POST['mypass'])
+       && !empty($_POST['myemail'])
+       && !empty($_POST['mypass'])
+    ){
+        $email=$_POST['myemail'];
+        $pass=$_POST['mypass'];
+        
+        
+        ///store the data to database
+        try{
+            ///PDO = PHP Data Object
+            $conn=new PDO("mysql:host=localhost:3306;dbname=kidsitter;","root","");
+            ///setting 1 environment variable
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            
+            $enc_password=md5($pass);
+            
+            ///checking from the database
+            $myquerystring="SELECT * FROM user WHERE email='$email' and pass='$enc_password'";
+            
+            $returnobj = $conn->query($myquerystring);   ///the return objects is a PDOStatement object
+            
+            if($returnobj->rowCount()==1){
+                
+                session_start();
+                $_SESSION['useremail']=$email; ///global variable
+                
+                
+                ?>
+                    <script>location.assign("home.php");</script>
+                <?php
+            }
+            else{
+                ?>
+                    <script>location.assign("SignIn.php");</script>
+                <?php
+            }
+        }
+        catch(PDOException $ex){
+            ?>
+                <script>location.assign("SignIn.php");</script>
+            <?php
+        }
+        
+    }
+    else{
+        ///if email and password data is empty or not set
+    ?>
+        <script>location.assign("SignIn.php");</script>
+    <?php
+        
+    } 
+    
+}
+else{
+    //we won't process
+    ?>
+        <script>location.assign('SignIn.php')</script>
+    <?php
+    
+}
+
+
+?>
