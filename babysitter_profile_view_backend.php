62b05db52b4b49cc79dd7b83672ac6fb88d2f1a1
Safiul Bari <45819810+safiulbari@users.noreply.github.com>
2022-05-14 04:27:21 +0600

    query json code added to backend file

--- babysitter_profile_view_backend.php
+++ babysitter_profile_view_backend.php
@@ -1,5 +1,28 @@
 <?php
 	error_reporting(0);
     require_once('dbconfig.php');
-    
+    $sitter_id = $_GET['sitter_id'];
+
+	$link = mysqli_connect(HOST, USER, PASS, DB );
+	
+	$status = 'OK';
+	$content = [];
+
+	if (mysqli_connect_errno()) {
+		$status = 'ERROR';
+		$content = mysqli_connect_error();
+	}
+
+	$query = "SELECT * FROM sitter where sitter_id = $sitter_id";
+
+	if ($result = mysqli_query($link, $query)) {
+		/* fetch associative array */
+		while ($row = mysqli_fetch_assoc($result)) {
+			$content[] = $row; // push value to array
+		}
+	}
+	$data = ["status" => $status, "content" => $content];
+
+	header('Content-type: application/json');
+	echo json_encode($data); // get all products in json format.
 ?>
