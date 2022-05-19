0286511940b8c5a7c189e5bbda6fee47fb8b49a9
IsratAfrin <sumon@example.com>
2022-05-20 01:29:11 +0600

    admin view sitter info half done

--- admin-sitter.php
+++ admin-sitter.php
@@ -33,6 +33,47 @@ $connect = mysqli_connect(HOST, USER, PASS, DB)
             <a class="link flex-center" href="#">My Profile</a>
         </div>
     </div>
+    </div>
+    </div>
+    <div class="table_responsive">
+        <table border="1">
+            <thead>
+                <tr>
+
+                    <th>first name</th>
+                    <th>last name</th>
+                    <th>Email</th>
+                    <th>Phone number</th>
+                    <th>License number</th>
+
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT *
+                        FROM users WHERE user_id=7";
+                $result = mysqli_query($connect, $sql);
+                $count = mysqli_num_rows($result);
+                $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                foreach ($row as $row) {
+                ?>
+                    <tr>
+
+                        <td> <?php echo $row['first_name']; ?> </td>
+                        <td> <?php echo $row['last_name']; ?> </td>
+                        <td> <?php echo $row['email']; ?> </td>
+
+
+
+                    </tr>
+                <?php
+                } ?>
+
+            </tbody>
+
+        </table>
+    </div>
 
 </body>
 
