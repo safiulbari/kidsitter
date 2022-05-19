54e1ee06fa182ea9f366890c30e4229deb9c3868
IsratAfrin <sumon@example.com>
2022-05-20 00:37:07 +0600

    interview database done

---
+++ timedb.php
@@ -0,0 +1,13 @@
+<?php
+require_once('dbconfig.php');
+$connect = mysqli_connect(HOST, USER, PASS, DB)
+    or die("Can not connect");
+if (isset($_POST['done'])) {
+    $date = date('Y-m-d', strtotime($_POST['datee']));
+    $time = date('h:i', strtotime($_POST['timee']));
+    $link = $_POST['link'];
+
+    mysqli_query($connect, "INSERT INTO interview (sitter_id, job_id, timee, datee, link)
+     VALUES(1, 10, '$time', '$date', '$link')")
+        or die("cant execute query");
+}
