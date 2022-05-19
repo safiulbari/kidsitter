678c72e74239883686635700f866ddd33e4899a6
shahnazfatima2 <sfatima152209@bscse.uiu.ac.bd>
2022-05-20 04:32:18 +0600

    initial comment

---
+++ index.py
@@ -0,0 +1,57 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Comment System using PHP and Ajax</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+ </head>
+ <body>
+  <br />
+  <h2 align="center"><a href="#">Comment System using PHP and Ajax</a></h2>
+  <br />
+  <div class="container">
+   <form method="POST" id="comment_form">
+    <div class="form-group">
+     <input type="text" name="comment_name" id="comment_name" class="form-control" placeholder="Enter Name" />
+    </div>
+    <div class="form-group">
+     <textarea name="comment_content" id="comment_content" class="form-control" placeholder="Enter Comment" rows="5"></textarea>
+    </div>
+    <div class="form-group">
+     <input type="hidden" name="comment_id" id="comment_id" value="0" />
+     <input type="submit" name="comment" id="submit" class="btn btn-info" value="Submit" />
+    </div>
+   </form>
+   <span id="comment_message"></span>
+   <br />
+   <div id="display_comment"></div>
+  </div>
+ </body>
+</html>
+
+<script>
+$(document).ready(function(){
+  $('#comment_form').on('submit',function(event)){
+  
+     event.preventDefault();
+     var form_data =$(this).serialize() #this smethod  will convert form data into ural code
+     $.ajax({
+          url:"add_comment.php",
+          method:"POST",
+          data: form_data,
+          dataType:"JSON"
+          success:function(data)
+          {
+               if(data.error !='')
+               {
+                  $('#comment_form')[0].reset();
+                  $('#comment_message').html(data.error);
+               
+               }
+          }
+  });
